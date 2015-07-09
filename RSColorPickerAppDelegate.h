1c4b154d03bcef91429531a2c74bcb8be10d6ee1
Bill Larsen <BillDev54@hotmail.com>
2015-07-09 19:27:12 +0200

    ImageManip
    Relation Bitmap

---
+++ RSColorPickerAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  RSColorPickerAppDelegate.h
+//  RSColorPicker
+//
+//  Created by Ryan Sullivan on 8/12/11.
+//  Copyright 2011 Freelance Web Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ColorPickerClasses/RSColorPickerView.h"
+
+@class RSBrightnessSlider;
+
+@interface RSColorPickerAppDelegate : NSObject <UIApplicationDelegate, RSColorPickerViewDelegate> {
+    NSString * savePath;
+}
+
+@property (nonatomic, strong) UIWindow *window;
+
+@property (nonatomic) RSColorPickerView *colorPicker;
+@property (nonatomic) RSBrightnessSlider *brightnessSlider;
+@property (nonatomic) UIView *colorPatch;
+
+@end
